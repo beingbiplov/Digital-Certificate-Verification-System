477d19e5a9471c7362ca7ab7dfacf90fce9d9f05
beingbiplov <sharmabiplov@gmail.com>
2025-12-20 20:09:12 +0100

    feat: add output for invoke ARN in Lambda module

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_arn" {
   value = aws_lambda_function.certificate_parser.arn
 }
+
+output "invoke_arn" {
+  value = aws_lambda_function.certificate_parser.invoke_arn
+}
