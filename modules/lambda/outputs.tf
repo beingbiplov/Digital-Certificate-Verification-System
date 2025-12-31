1eede0324a5ba632cc26546b1e59e9aa256e476c
beingbiplov <sharmabiplov@gmail.com>
2025-12-31 15:52:58 +0100

    fix: update outputs to reference the correct presign Lambda function

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,11 +1,11 @@
 output "lambda_function_name" {
-  value = aws_lambda_function.certificate_parser.function_name
+  value = aws_lambda_function.certificate_presign.function_name
 }
 
 output "lambda_arn" {
-  value = aws_lambda_function.certificate_parser.arn
+  value = aws_lambda_function.certificate_presign.arn
 }
 
 output "invoke_arn" {
-  value = aws_lambda_function.certificate_parser.invoke_arn
+  value = aws_lambda_function.certificate_presign.invoke_arn
 }
