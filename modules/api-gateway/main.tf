2cb86f4a5c962918c28f619179b49f7b0677a43f
beingbiplov <sharmabiplov@gmail.com>
2026-01-01 11:44:44 +0100

    fix: remove duplicate api gateway stage

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -57,7 +57,19 @@ resource "aws_api_gateway_stage" "stage" {
   stage_name    = var.environment
   rest_api_id   = aws_api_gateway_rest_api.certificate_api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigw_logs.arn
+    format = jsonencode({
+      requestId   = "$context.requestId"
+      httpMethod  = "$context.httpMethod"
+      resource    = "$context.resourcePath"
+      status      = "$context.status"
+      error       = "$context.error.message"
+    })
+  }
 }
+
 
 # Permission for API Gateway to invoke Lambda
 resource "aws_lambda_permission" "allow_apigw" {
