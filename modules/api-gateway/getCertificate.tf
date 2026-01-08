9d9f8f8024ad0433822659b074fababfbc73800b
beingbiplov <sharmabiplov@gmail.com>
2026-01-08 22:53:45 +0100

    feat: add cors support for get api

--- modules/api-gateway/getCertificate.tf
+++ modules/api-gateway/getCertificate.tf
@@ -12,24 +12,24 @@ resource "aws_api_gateway_method" "get_certificate" {
 }
 
 resource "aws_api_gateway_integration" "get_certificate_integration" {
-  rest_api_id = aws_api_gateway_rest_api.certificate_api.id
-  resource_id = aws_api_gateway_resource.certificate_id.id
-  http_method = aws_api_gateway_method.get_certificate.http_method
-
+  rest_api_id             = aws_api_gateway_rest_api.certificate_api.id
+  resource_id             = aws_api_gateway_resource.certificate_id.id
+  http_method             = aws_api_gateway_method.get_certificate.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.get_certificate_lambda_invoke_arn
 }
 
+# Lambda permission for API Gateway GET
 resource "aws_lambda_permission" "allow_apigw_get" {
   statement_id  = "AllowAPIGatewayInvokeGetCertificate"
   action        = "lambda:InvokeFunction"
   function_name = var.get_certificate_lambda_function_name
   principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.certificate_api.execution_arn}/*/GET/certificates/*"
+  source_arn    = "${aws_api_gateway_rest_api.certificate_api.execution_arn}/*/GET/certificates/*"
 }
 
+# OPTIONS for CORS
 resource "aws_api_gateway_method" "options_certificate_id" {
   rest_api_id   = aws_api_gateway_rest_api.certificate_api.id
   resource_id   = aws_api_gateway_resource.certificate_id.id
@@ -41,8 +41,7 @@ resource "aws_api_gateway_integration" "options_certificate_id" {
   rest_api_id = aws_api_gateway_rest_api.certificate_api.id
   resource_id = aws_api_gateway_resource.certificate_id.id
   http_method = aws_api_gateway_method.options_certificate_id.http_method
-
-  type = "MOCK"
+  type        = "MOCK"
 
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
@@ -70,9 +69,8 @@ resource "aws_api_gateway_integration_response" "options_certificate_id_200" {
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
   }
 }
-
 
